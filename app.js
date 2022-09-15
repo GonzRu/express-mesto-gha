b72d405f364a0959f3f584a34f2b1afba64a064e
Denis Smirnov <gonzru@gmail.com>
2022-09-15 18:09:53 +0300

    move auth to app.js (required by tests)

--- app.js
+++ app.js
@@ -10,7 +10,13 @@ const app = express();
 mongoose.connect('mongodb://localhost:27017/mesto');
 
 app.use(cors());
-app.use(require('./midlewares/auth'));
+app.use((req, res, next) => {
+  req.user = {
+    _id: '6323082955e2de14704d5966',
+  };
+
+  next();
+});
 app.use(require('body-parser').json());
 app.use(require('./routes/auth'));
 app.use(require('./routes/users'));
--- midlewares/auth.js
+++
@@ -1,7 +0,0 @@
-module.exports = (req, res, next) => {
-  req.user = {
-    _id: '6323082955e2de14704d5966',
-  };
-
-  next();
-};
