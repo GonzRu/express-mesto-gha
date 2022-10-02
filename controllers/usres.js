892c1286d8305742021373c7f1c57be7290637fa
Denis Smirnov <gonzru@gmail.com>
2022-10-02 13:44:20 +0600

    update signup validation

--- controllers/usres.js
+++ controllers/usres.js
@@ -23,8 +23,8 @@ module.exports.createUser = [
     body: Joi.object().keys({
       email: Joi.string().required().email(),
       password: Joi.string().required().min(2),
-      name: Joi.string(),
-      about: Joi.string(),
+      name: Joi.string().min(2).max(30),
+      about: Joi.string().min(2).max(30),
       avatar: Joi.string().uri(),
     }),
   }),
