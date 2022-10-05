e1149b1e92423d4d65bedf6a6bd07ab34dd94fe4
Denis Smirnov <gonzru@gmail.com>
2022-10-05 12:31:35 +0600

    fix CreateCard required link validation

--- routes/cards.js
+++ routes/cards.js
@@ -11,7 +11,7 @@ router.post(
   celebrate({
     body: Joi.object().keys({
       name: Joi.string().required().min(2).max(30),
-      link: Joi.string().custom(linkValidator),
+      link: Joi.string().required().custom(linkValidator),
     }),
   }),
   createCard,
