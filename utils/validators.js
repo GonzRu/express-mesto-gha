eb33a51d7ae88931572e217d445ad100d06db724
Denis Smirnov <gonzru@gmail.com>
2022-10-05 12:20:12 +0600

    fix linkValidator

--- utils/validators.js
+++ utils/validators.js
@@ -2,6 +2,7 @@ const validator = require('validator');
 
 module.exports.linkRegExp = /(https|http):\/\/(www\.)?([a-zA-Z\d-]+)\.(\w{2,4})([\w\-._~:/?#[\]@!$&'()*+,;=]*)/;
 
-module.exports.linkValidator = (value, helpers) => (validator.isURL(value)
-  ? true
-  : helpers.message('Imvalid link'));
+module.exports.linkValidator = (value, helpers) => {
+  if (validator.isURL(value)) return value;
+  return helpers.message('Invalid link');
+};
